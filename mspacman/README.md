d2b0db37d415879c7e72adae8b00486182e5bda1
khuyenbui <kenshinvnzzz@gmail.com>
2015-03-03 14:20:29 -0500

    Completed. Edit game.js to draw on canvas. Added README

---
+++ mspacman/README.md
@@ -0,0 +1,9 @@
+README for ms-pacman
+
+Identify what aspects of the work have been correctly implemented and what have not.
+    All well done: drawn blue Google board and Mspacman on top on the canvas.
+Identify anyone with whom you have collaborated or discussed the assignment.
+    Thank you Barry for helping me with the code.
+
+Say approximately how many hours you have spent completing the assignment.
+    30'
--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,13 @@
-// Replace this with your code...
+function init()
+{
+        canvas = document.getElementById('game_canvas');
+        ctx = canvas.getContext('2d');
+        pacmanImg = new Image();
+        pacmanImg.src = 'pacman10-hp-sprite.png';
+        pacmanImg.onload = function draw() {
+                ctx.drawImage(pacmanImg, 320, 5, 460, 135, 0, 0, 460, 135);
+                ctx.drawImage(pacmanImg, 80, 20, 20, 20, 35, 30, 20, 20);
+        }
+}
+
+// lesson learned: don't declare image object as var.
