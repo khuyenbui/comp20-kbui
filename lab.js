695b111e4b3b659eb0c3fb3d1da59c42125b2738
khuyenbui <kenshinvnzzz@gmail.com>
2015-03-09 16:13:28 -0400

    added lab.js, using XMLHttpRequest

--- lab.js
+++ lab.js
@@ -1,6 +1,10 @@
+// my mind hasn't been in CS recently.
+// XMLHtmlRequest: an object to make request.
+// JS doesn't have a func to open / write file => must use HTTP get then.
+
 function parse() {
         // step 1: create a new instance
-        request = new XMLHttpRequest();
+        request = new XMLHttpRequest(); // why isn't this var?
 
         // step 2: open HTTP request
         request.open("GET", "data.json", true); // last arg: ?async?
@@ -8,14 +12,14 @@ function parse() {
                 // NO: no
 
         // step 3: set up way to manage the response
-        request.onreadystatechange = parse; // whut is this?
+        request.onreadystatechange = parseData; // whut is this?
 
         // step 4: execute the request. (ALTHOUGH IT READS "SEND" - "fire away"
         request.send();
 
 }
 
-function parse(){
+function parseData(){
         if (request.readyState == 4 && request.status == 200) { // good respnse
                 alert(request.responseText);
                 converted = JSON.parse(request.responseText);
